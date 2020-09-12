8ba16fed385e0af06fc5cd9acf2a33071a67113f
Chuck Collins <CCollins009@gmail.com>
2020-09-12 13:39:02 -0400

    New Application

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+ # Project 1
++ By: Chuck Collins
++ Production URL: <http://e2p1.chuckac.me>
+
+## Outside resources
+n/a
+
+## Notes for instructor
+n/a
---
+++ p1/index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang='en'>
+<head><Title>Project 1</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <h1>Project 1</h1>
+</body>
+</html>
